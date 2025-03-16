8b56e99a63f09c53ede439d197c898499612ddbf
Gitmoid <vrana.vojtech@post.cz>
2025-03-16 10:59:16 +0100

    Security: JWT Authentication Filter - implemented JwtAuthenticationFilter

--- src/main/java/com/devtiro/blog/services/AuthenticationService.java
+++ src/main/java/com/devtiro/blog/services/AuthenticationService.java
@@ -3,6 +3,7 @@ package com.devtiro.blog.services;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface AuthenticationService {
-    UserDetails authenticate(String email, String password);
-    String generateToken(UserDetails userDetails);
+    UserDetails authenticate(String email, String password); // returns UserDetails by authenticating user email and password
+    String generateToken(UserDetails userDetails); // turns UserDetails into JWT
+    UserDetails validateToken(String token); // turns JWT into UserDetails
 }
