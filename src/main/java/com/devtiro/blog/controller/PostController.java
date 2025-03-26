15934a677c8807a608b36887b9e50a69bd8ce081
Gitmoid <vrana.vojtech@post.cz>
2025-03-26 01:49:28 +0100

    Post Endpoints: Get Post Endpoint

--- src/main/java/com/devtiro/blog/controller/PostController.java
+++ src/main/java/com/devtiro/blog/controller/PostController.java
@@ -65,6 +65,16 @@ public class PostController {
         UpdatePostRequest updatePostRequest = postMapper.toUpdatePostRequest(updatePostRequestDto);
         Post updatedPost = postService.updatePost(id, updatePostRequest);
         PostDto updatedPostDto = postMapper.toDto(updatedPost);
+
         return ResponseEntity.ok(updatedPostDto);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<PostDto> getPost(
+            @PathVariable UUID id) {
+        Post post = postService.getPost(id);
+        PostDto postDto = postMapper.toDto(post);
+
+        return ResponseEntity.ok(postDto);
     }
 }
